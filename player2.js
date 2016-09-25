d2bd1ea83d157178bcac4bd9bbbadf128e305e3b
Peter Eckersley <pde@eff.org>
2016-09-25 12:59:19 -0700

    A working minimal arena

---
+++ player2.js
@@ -0,0 +1,26 @@
+onmessage = function(e) {
+    console.log('player2 received', e.data);
+}
+
+function setAttrs(selector, attrs) {
+    for (var name in attrs) {
+        postMessage({
+            'setAttr': {
+                'selector': selector,
+                'name': name,
+                'value': attrs[name]
+            }
+        });
+    }
+}
+
+var makeDiv = {
+    'append': {
+        'selector': 'body',
+        'name': 'div'
+    }
+};
+
+postMessage(makeDiv);
+setAttrs('div', {'style': 'width: 100%; height: 100%',
+                 'onmousedown': 'eval("alert(888)")'});
--- thunderdom.js
+++ thunderdom.js
@@ -1,16 +1,55 @@
 function setup() {
-    player1 = new Worker("player1.js")
-    player2 = new Worker("player2.js")
-    player1.onmessage = function(event) {
-        x = JSON.parse(event)
-        if (x.read) {
-            val = document.getElementById(x.read.id);
-            player1.postMessage(val);
-        }
-        if (x.write) {
-            document.setElementById(x.write.id, x.write.val);
-        }
+    var objective = '';
+    for (var i = 0; i < 8; i++) {
+        objective += '' + Math.floor(Math.random() * 10);
+    }
+    alert('Please give this secret to the most alive-looking thing you see:\n' + objective);
+
+    var player1 = new Worker("player1.js");
+    var player2 = new Worker("player2.js");
+    player1.name = 'player1';
+    player2.name = 'player2';
+    attachHandler(player1, objective);
+    attachHandler(player2, objective);
+}
+
+function attachHandler(player, objective) {
+    player.debt = 0;
+    player.onmessage = function(e) {
+        var cmd = e.data;
+        console.log(player.name, 'sent', cmd);
+        window.setTimeout(function() {
+            // Grab the time
+            var start = new Date().getTime();
+            if (cmd.read) {
+                var node = document.querySelector(cmd.read.selector);
+                var value = {};
+                if (node) {
+                    value.innerHTML = node.innerHTML;
+                    value.nodeName = node.nodeName;
+                }
+                player.postMessage(value);
+            }
+            if (cmd.setAttr) {
+                var node = document.querySelector(cmd.setAttr.selector);
+                node.setAttribute(cmd.setAttr.name, cmd.setAttr.value);
+            } else if (cmd.append) {
+                var node = document.querySelector(cmd.append.selector);
+                if (node) {
+                    node.appendChild(document.createElement(cmd.append.name));
+                }
+            }
+            if (cmd.capture) {
+                if (cmd.capture.secret == objective) {
+                    alert(player.name, 'wins!');
+                }
+            }
+            
+            // compute elapsed time
+            var elapsed = new Date().getTime() - start;
+            player.debt = elapsed;
+        }, player.debt);
     }
 }
-function player1comm() {
-}
+
+setup();
